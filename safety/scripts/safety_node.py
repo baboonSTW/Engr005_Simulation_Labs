b0b62a841d5cfaea9de043416f7092d89f4167f9
Weihang Guo <weg223@lehigh.edu>
2022-07-28 13:44:07 -0700

    initial commit

---
+++ safety/scripts/safety_node.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# license removed for brevity
+# Course: ECE350
+# Team: 3
+# Author: Weihang Guo
+# Date: 2/28/2022
+
+import rospy
+import numpy as np
+from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
+from std_msgs.msg import Bool
+from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
+# TODO: import ROS msg types and libraries
+
+class Safety(object):
+    
+if __name__ == '__main__':
+    main()
---
+++ wall_follow/scripts/left_wall_follow.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# Team 3
+# Weihang Guo
+# Race 1
+from __future__ import print_function
+import sys
+import math
+import numpy as np
+
+#ROS Imports
+import rospy
+from sensor_msgs.msg import Image, LaserScan
+from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
+
+class WallFollow:
+
+if __name__=='__main__':
+	main()
